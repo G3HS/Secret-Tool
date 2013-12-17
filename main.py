473a160e677de9c1991ebed2df97bf80ef77765f
thekaratekid552 <aiudirog@github.com>
2013-12-17 11:33:08 -0500

    Had to put everything in one file to allow the rom to be easily shared between the objects.

--- main.py
+++ main.py
@@ -1,13 +1,12 @@
 import wx, os
-from TabbedDataPanel import *
+#from TabbedDataPanel import *
 
 OPEN = 1
-open_rom = None
 
 class MainWindow(wx.Frame):
     def __init__(self, parent, title):
         wx.Frame.__init__(self, parent, title=title, size=(800,600))
-        
+        self.open_rom = None
         panel = wx.Panel(self)
         
         tabbed_area = TabbedEditorArea(panel)
@@ -44,7 +43,117 @@ class MainWindow(wx.Frame):
         if open_dialog.ShowModal() == wx.ID_OK:
             filename = open_dialog.GetPath()
             with open(filename, "r+b") as file:
-                open_rom = file
+                self.open_rom = file
+                
+class TabbedEditorArea(wx.Notebook):
+    def __init__(self, parent):
+        wx.Notebook.__init__(self, parent, id=wx.ID_ANY, style=
+                                             wx.BK_DEFAULT
+                                             #wx.BK_TOP 
+                                             #wx.BK_BOTTOM
+                                             #wx.BK_LEFT
+                                             #wx.BK_RIGHT
+                                             )
+        
+        PokeDataTab = PokemonDataEditor(self)
+        PokeDataTab.SetBackgroundColour("Gray")
+        self.AddPage(PokeDataTab, "Pokemon Data Editor")
+        
+        
+        self.Bind(wx.EVT_NOTEBOOK_PAGE_CHANGED, self.OnPageChanged)
+        self.Bind(wx.EVT_NOTEBOOK_PAGE_CHANGING, self.OnPageChanging)
+        
+    def OnPageChanged(self, event):
+        old = event.GetOldSelection()
+        new = event.GetSelection()
+        sel = self.GetSelection()
+        #print 'OnPageChanged,  old:%d, new:%d, sel:%d\n' % (old, new, sel)
+        event.Skip()
+
+    def OnPageChanging(self, event):
+        old = event.GetOldSelection()
+        new = event.GetSelection()
+        sel = self.GetSelection()
+        #print 'OnPageChanging, old:%d, new:%d, sel:%d\n' % (old, new, sel)
+        event.Skip()
+        
+class PokemonDataEditor(wx.Panel):
+    #This tab will allow editing of Pokemon Stats, moves, etc
+    def __init__(self, parent):
+        wx.Panel.__init__(self, parent=parent, id=wx.ID_ANY)
+        tabbed_area = DataEditingTabs(self)
+        sizer = wx.BoxSizer(wx.VERTICAL)
+        sizer.Add(tabbed_area, 1, wx.ALL|wx.EXPAND, 5)
+        self.SetSizer(sizer)
+        self.Layout()
+        
+class DataEditingTabs(wx.Notebook):
+    def __init__(self, parent):
+        wx.Notebook.__init__(self, parent, id=wx.ID_ANY, style=
+                                             wx.BK_DEFAULT
+                                             #wx.BK_TOP 
+                                             #wx.BK_BOTTOM
+                                             #wx.BK_LEFT
+                                             #wx.BK_RIGHT
+                                             )
+        sizer = wx.BoxSizer(wx.VERTICAL)
+        
+        stats = StatsTab(self)
+        moves = MovesTab(self)
+        evo = EvoTab(self)
+        dex = PokeDexTab(self)
+        
+        self.AddPage(stats, "Stats")
+        self.AddPage(moves, "Moves")
+        self.AddPage(evo, "Evolutions")
+        self.AddPage(dex, "PokeDex")
+        
+        self.SetSizer(sizer)
+        self.Bind(wx.EVT_NOTEBOOK_PAGE_CHANGED, self.OnPageChanged)
+        self.Bind(wx.EVT_NOTEBOOK_PAGE_CHANGING, self.OnPageChanging)
+        
+    def OnPageChanged(self, event):
+        old = event.GetOldSelection()
+        new = event.GetSelection()
+        sel = self.GetSelection()
+        
+        #print 'OnPageChanged,  old:%d, new:%d, sel:%d\n' % (old, new, sel)
+        event.Skip()
+
+    def OnPageChanging(self, event):
+        old = event.GetOldSelection()
+        new = event.GetSelection()
+        sel = self.GetSelection()
+        #print 'OnPageChanging, old:%d, new:%d, sel:%d\n' % (old, new, sel)
+        event.Skip()
+        
+class StatsTab(wx.Panel):
+    def __init__(self, parent):
+        wx.Panel.__init__(self, parent=parent, id=wx.ID_ANY)
+        sizer = wx.BoxSizer(wx.VERTICAL)
+
+        self.SetSizer(sizer)
+
+class MovesTab(wx.Panel):
+    def __init__(self, parent):
+        wx.Panel.__init__(self, parent=parent, id=wx.ID_ANY)
+        sizer = wx.BoxSizer(wx.VERTICAL)
+
+        self.SetSizer(sizer)
+
+class EvoTab(wx.Panel):
+    def __init__(self, parent):
+        wx.Panel.__init__(self, parent=parent, id=wx.ID_ANY)
+        sizer = wx.BoxSizer(wx.VERTICAL)
+
+        self.SetSizer(sizer)
+
+class PokeDexTab(wx.Panel):
+    def __init__(self, parent):
+        wx.Panel.__init__(self, parent=parent, id=wx.ID_ANY)
+        sizer = wx.BoxSizer(wx.VERTICAL)
+
+        self.SetSizer(sizer)
                 
 app = wx.App(False)
 frame = MainWindow(None, "Pokemon Gen III Hacking Suite")
