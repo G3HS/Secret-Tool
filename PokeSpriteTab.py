c844e9dbf8041ec5c47686383d4b23d8551bb327
thekaratekid552 <aiudirog@github.com>
2014-01-12 18:46:25 -0500

    Implementing Pygame position changer.

--- PokeSpriteTab.py
+++ PokeSpriteTab.py
@@ -1,12 +1,15 @@
 from LZ77 import *
 from ImageFunctions import *
 
+import pygame
+pygame.init()
+        
 class SpriteTab(wx.Panel):
     def __init__(self, parent, rom=None):
         wx.Panel.__init__(self, parent=parent, id=wx.ID_ANY)
         self.rom = rom
         
-        self.sizer = wx.BoxSizer(wx.HORIZONTAL)
+        self.sizer = wx.BoxSizer(wx.VERTICAL)
         self.generate_UI()
         
         self.SetSizer(self.sizer)
@@ -24,8 +27,32 @@ class SpriteTab(wx.Panel):
         
         pal = ConvertGBAPalTo25Bit(palette)
         bmp = ConvertGBAImageToNormal(image,pal)
-        
+        bmp.SaveFile("Resources\\PokeFront.png", wx.BITMAP_TYPE_PNG)
         
         static_image = wx.StaticBitmap(self,-1,bmp)
+        self.PositionEditor()
         
-        self.sizer.Add(static_image, 1, wx.LEFT | wx.BOTTOM | wx.TOP, 10)
+    def PositionEditor(self):
+        #origin for pokeback is (40,48)
+        #origin for pokefront is (144,8)
+        
+        pygame.display.set_caption("Position Editor", "Resources\\PokeSquare.png")
+        pygame.display.set_icon(pygame.image.load("Resources\\PokeSquare.png"))
+        size = width, height = 240,160
+        screen = pygame.display.set_mode(size)
+        BG = pygame.image.load("Resources\\BattleBG.png")
+        
+        PokeFront = pygame.image.load("Resources\\PokeFront.png")
+        transparent_color = PokeFront.get_at((0,0))
+        PokeFront.set_colorkey(transparent_color)
+        
+        TEXTBOX = pygame.image.load("Resources\\BattleTextBox.png")
+        
+        screen.blit(BG,pygame.Rect(0,0,240,160))
+        screen.blit(PokeFront,pygame.Rect(144,8,64,64))
+        screen.blit(TEXTBOX,pygame.Rect(0,0,240,160))
+        pygame.display.flip()
+        
+        
+        
+        
