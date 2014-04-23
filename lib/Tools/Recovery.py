0c7b42ee7df6bb4130736c28635131efc262b7d9
thekaratekid552 <thekaratekid552@gmail.com>
2014-04-23 00:47:20 -0400

    Filled out Recovery Ini and fixed some errors in PokeRoms.ini

--- lib/Tools/Recovery.py
+++ lib/Tools/Recovery.py
@@ -1,4 +1,19 @@
+from lib.Tools.RecoveryINI import *
+from GLOBALS import *
+
 class Recovery:
-    def __init__(rom):
-        rom.seek(0xAC)
-        self.gamecode = rom.read(4)
+    def __init__()):
+        with open(GLOBALS.OpenRomName, "r+b") as rom:
+            rom.seek(0xAC)
+            self.gamecode = rom.read(4)
+            if self.gamecode not in RecoveryIni.INI:
+                self.NoRecovery = True
+                return
+            else:
+                self.NoRecovery = False
+                self.INI = RecoveryIni.INI[self.gamecode]
+                self.Recover(rom)
+            
+    def Recover(rom):
+        for name, offset in self.INI.iteritems():
+            pass
