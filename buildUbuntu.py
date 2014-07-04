cd5e76ea8494e5506f8ee4ef648876b404e1b83b
thekaratekid552 <thekaratekid552@gmail.com>
2014-07-04 01:01:26 -0400

    cleaning

---
+++ buildUbuntu.py
@@ -0,0 +1,9 @@
+from os import system,remove
+from shutil import copyfile,copytree
+
+system("pyinstaller","mainUbuntu.spec")
+remove("E:\Secret-Tool\build")
+copyfile("E:\Secret-Tool\G3HS_Documentation.pdf","E:\Secret-Tool\mainWIN.spec\dist")
+copyfile("E:\Secret-Tool\PokeRoms.ini","E:\Secret-Tool\mainWIN.spec\dist")
+copyfile("E:\Secret-Tool\Finish.bat","E:\Secret-Tool\mainWIN.spec\dist")
+copytree("E:\Secret-Tool\Resources","E:\Secret-Tool\mainWIN.spec\dist")
--- buildWindows.bat
+++
@@ -1,6 +0,0 @@
-START pyinstaller E:\Secret-Tool\mainWIN.spec
-rd E:\Secret-Tool\mainWIN.spec /s/q
-COPY "E:\Secret-Tool\G3HS_Documentation.pdf" "E:\Secret-Tool\mainWIN.spec\dist"
-COPY "E:\Secret-Tool\PokeRoms.ini" "E:\Secret-Tool\mainWIN.spec\dist"
-COPY "E:\Secret-Tool\Finish.bat" "E:\Secret-Tool\mainWIN.spec\dist"
-XCOPY "E:\Secret-Tool\Resources" "E:\Secret-Tool\mainWIN.spec\dist" /D /E /C /R /I /K /Y 
